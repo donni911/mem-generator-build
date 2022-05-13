ac06c867b3e92fdbc3a43965a852439ff6247d4f
donni911 <rodion2001vo@gmail.com>
2022-05-13 13:14:25 +0300

    add task result

---
+++ readme.md
@@ -0,0 +1,44 @@
+# TASK RESULT
+
+## I use the following technologies / frameworks in my project:
+
+- HTML
+- CSS
+
+- JAVASCRIPT
+
+- GULP
+
+- NPM
+
+1.  I choosed Javascript , because it's a programming language that I know(good level),also I was thinking to use vue.js , but I have not good experience in using Vue.
+
+2.  I use npm to download modules that I need.
+
+3.  I create build via gulp few month ago, so I decide create
+    mem-generetor with it. It gives a lot of opportunities such as:
+
+- css to scss
+- minify js,css
+
+- build for production or build zip-file
+
+### Installation / Run
+
+#### The following components must be installed locally:
+
+- gulp: CLI version: 2.3.0
+
+- Local version: 4.0.2
+
+- nodejs v17.9.0
+
+To run the project locally, enter the following in the command line / bash:
+
+$ git clone <linktorepository>
+
+$ cd ./mem-generator
+
+$ npm install
+
+$ gulp || npm run dev
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,80 @@
-import * as flsFunctions from "./modules/functions.js"
+import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
+
+const imageInput = document.getElementById("imageInput");
+const canvas = document.getElementById("mem");
+const topText = document.getElementById("topText");
+const bottomText = document.getElementById("bottomText");
+
+let image;
+
+imageInput.addEventListener("change", (e) => {
+  const imageDataUrl = URL.createObjectURL(e.target.files[0]);
+
+  image = new Image();
+  image.src = imageDataUrl;
+
+  image.addEventListener(
+    "load",
+    () => {
+      updateMemeCanvas(canvas, image, topText.value, bottomText.value);
+    },
+    { once: true }
+  );
+});
+
+topText.addEventListener("change", () => {
+  updateMemeCanvas(canvas, image, topText.value, bottomText.value);
+});
+
+bottomText.addEventListener("change", () => {
+  updateMemeCanvas(canvas, image, topText.value, bottomText.value);
+});
+
+function updateMemeCanvas(canvas, image, topText, bottomText) {
+  const ctx = canvas.getContext("2d");
+  const width = image.width;
+  const height = image.height;
+  const fontSize = Math.floor(width / 10);
+  const yOffset = height / 25;
+
+  // Update canvas background
+  canvas.width = width;
+  canvas.height = height;
+  ctx.drawImage(image, 0, 0);
+
+  // Prepare text
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = Math.floor(fontSize / 4);
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.lineJoin = "round";
+  ctx.font = `${fontSize}px sans-serif`;
+
+  // Add top text
+  ctx.textBaseline = "top";
+  ctx.strokeText(topText, width / 2, yOffset);
+  ctx.fillText(topText, width / 2, yOffset);
+
+  // Add bottom text
+  ctx.textBaseline = "bottom";
+  ctx.strokeText(bottomText, width / 2, height - yOffset);
+  ctx.fillText(bottomText, width / 2, height - yOffset);
+
+  let imgToPng = canvas.toDataURL("image/png");
+
+  let imgToJpg = canvas.toDataURL("image/jpg");
+  let imgToGif = canvas.toDataURL("image/gif");
+
+  let downloadImg = document.querySelector(".mem-download__wrapper");
+
+  downloadImg.innerHTML =
+    '         <button class="mem-download__button">Download</button>    <ul class="mem-download"> <li class="mem-download__item"> <a download="mem.png" href="' +
+    imgToPng +
+    '">Download PNG</a></li> <li class="mem-download__item"><a download="mem.jpg" href="' +
+    imgToJpg +
+    '">Download JPG</a> </li> <li class="mem-download__item"><a download="mem.gif" href="' +
+    imgToGif +
+    '">Download GIF</a></li> </ul> ';
+}
